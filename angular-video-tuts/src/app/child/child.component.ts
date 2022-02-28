686b90a7038c611552550dd7f94aea12f41cd345
hkg328 <hkg328@outlook.com>
2022-02-28 05:58:42 -0800

    pass child to parent / parent to child

---
+++ angular-video-tuts/src/app/child/child.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss']
+})
+export class ChildComponent implements OnInit {
+
+  @Input()
+  childMessage: string = "Default message from child";
+
+  @Output() messageEvent = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sendToParent () {
+    console.log('it worked for function');
+    this.messageEvent.emit("test message from child");
+  }
+
+}
